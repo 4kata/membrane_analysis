457b1a615603a41faced4b43c92e39fc19ff94e6
4kata <82141225+4kata@users.noreply.github.com>
2024-05-24 13:48:24 +0900

    Add files via upload

---
+++ build/01_build_xdr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FC=ifort
+CXX=icc
+
+cd ../
+tar -xzvf xdrfile-1.1.4.tar.gz
+cd xdrfile-1.1.4
+CDIR=`pwd`
+
+./configure FC=${FC} CXX=${CXX} --prefix=${CDIR}/../xdr
+
+make
+make install
+
+cd ../xdr
---
+++ build/02_make_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../
+cd programs
+
+program_list=`ls`
+echo ${program_list}
+filtered_list=$(echo "$program_list" | sed '/bin/d; /mod/d')
+echo $filtered_list
+IFS=$'\n' read -r -d '' -a filtered_array <<<"$filtered_list"
+rm -f ./bin/${filtered_array}
+
+for i_dir in "${filtered_array[@]}"
+do 
+  cd ./${i_dir}
+  rm -f ${i_dir}
+  make
+  cd ../
+done
+cd ./bin
+
+
